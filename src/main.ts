30887e381f76902f1f7ef61d1213a7c6402ddb6e
Maziar Zendehroudi <maziarzendehroudi@gmail.com>
2026-08-24 13:23:48 +0200

    fix: clean duplicate imports in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './style.css'
-import { GameEngine } from './game/GameEngine'
+import './style.css';
+import { GameEngine } from './game/GameEngine';
 
-// Récupération du Canvas principal
 const canvas = document.getElementById('clockCanvas') as HTMLCanvasElement;
-
-// Lancement du Moteur de Jeu Interactif
-new GameEngine(canvas);
+if (canvas) {
+  new GameEngine(canvas);
+}
